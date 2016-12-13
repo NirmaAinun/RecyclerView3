3fdbf821c20e4994c59c0dcb60fe799679da98f4
Nirmala Ainun <nirmala.ainun.na@gmail.com>
2016-12-14 05:56:17 +0700

    RecyclerView Add

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/Adapter/HotelAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/Adapter/HotelAdapter.java
@@ -1,4 +1,4 @@
-package id.sch.smktelkom_mlg.learn.recyclerview3.Adapter;
+package id.sch.smktelkom_mlg.learn.recyclerview3.adapter;
 
 /**
  * Created by Nirmala Ainun on 14/12/2016.
--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
@@ -1,4 +1,4 @@
-package id.sch.smktelkom_mlg.learn.recyclerview3.Hotel;
+package id.sch.smktelkom_mlg.learn.recyclerview3.model;
 
 /**
  * Created by Nirmala Ainun on 14/12/2016.
